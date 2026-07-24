cfb7fa20718cd1cec5eeb2da25ee55d83cdc58ea
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-07-24 21:23:02 +0530

    Attached Payloads to Actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,6 +11,12 @@ const Counter = () => {
     });
   };
 
+  const increaseHandler = () => {
+    dispatch({
+      type:'increase', amount:5
+    })
+  }
+
   const decrementHandler = () => {
     dispatch({
       type: 'decrement'
@@ -25,6 +31,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
